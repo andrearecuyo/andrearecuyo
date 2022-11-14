b24edaf9202b2a5d01e302430e435df06fe5bf71
andrea_recuyo <andrearecuyo@beehive.hk>
2022-11-15 02:46:25 +0800

    Updated for my portfolio.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactFullpage from '@fullpage/react-fullpage';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Works from './components/Works';
+import Contact from './components/Contact';
 
 function App() {
+  const anchors = ['hello', 'about', 'skills', 'works', 'contact'];
+  const Fullpage = () => (
+    <ReactFullpage
+      //fullpage options
+      anchors={anchors}
+      navigation
+      navigationTooltips={anchors}
+      licenseKey = {'YOUR_KEY_HERE'}
+      scrollingSpeed = {1000}
+  
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <Header />
+              <Hero />
+            </div>
+            <div className="section about">
+              <About />
+            </div>
+            <div className="section">
+              <Skills />
+            </div>
+            <section className="section works">
+              <Works />
+            </section>
+            <section className="section contact">
+              <Contact />
+            </section>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fullpage />
     </div>
   );
 }
